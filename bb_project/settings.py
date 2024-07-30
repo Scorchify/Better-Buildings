e32298038303e1e63ebe073887ed0e46e414eb2f
Scorchify <139656419+Scorchify@users.noreply.github.com>
2024-07-30 01:18:57 -0400

    fixed bootstrap

--- bb_project/settings.py
+++ bb_project/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [

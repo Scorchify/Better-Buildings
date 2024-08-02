433079ea913b85d2cece469b91441ad90ad7f692
Scorchify <139656419+Scorchify@users.noreply.github.com>
2024-08-02 00:59:51 -0400

    some styling done, but I need to go to sleep so I'll finish rest of it tmrw.

--- better_buildings/forms.py
+++ better_buildings/forms.py
@@ -3,12 +3,20 @@ from django import forms
 from .models import Area, Report
 
 class AreaForm(forms.ModelForm):
+    class Media:
+        css = {
+            "all": ["css/input.css"]
+        }
     class Meta:
         model = Area
         fields = ['text']
         labels = {'text': ''}
 
 class ReportForm(forms.ModelForm):
+    class Media:
+        css = {
+            "all": ["css/input.css"]
+        }
     area = forms.ModelChoiceField(Area.objects.all())
     
     class Meta:

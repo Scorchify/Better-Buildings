8feeecf31e84e711b6187e14e14190b0b2acad8d
Scorchify <139656419+Scorchify@users.noreply.github.com>
2024-08-25 00:34:39 -0400

    1 bug fix

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,21 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, School
 
 class CustomUserAdmin(UserAdmin):
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('display_name', 'school')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'password1', 'password2'),
+            'fields': ('username', 'password1', 'password2', 'display_name', 'school'),
         }),
     )
-    list_display = ('username', 'email', 'is_staff')
-    search_fields = ('username', 'email')
+    list_display = ('username', 'email', 'display_name', 'school', 'is_staff')
+    search_fields = ('username', 'email', 'display_name', 'school__name')
     ordering = ('username',)
 
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(School)

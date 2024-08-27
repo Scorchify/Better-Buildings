9654ce846be0aecd6eb612100c488ffbb30419c9
Scorchify <139656419+Scorchify@users.noreply.github.com>
2024-08-26 22:02:06 -0400

    nevermind should work now forgot about gdal

--- bb_project/settings.py
+++ bb_project/settings.py
@@ -3,6 +3,11 @@ import dj_database_url
 import os
 
 
+#gps
+if 'GDAL_LIBRARY_PATH' in os.environ:
+    GDAL_LIBRARY_PATH = os.environ['GDAL_LIBRARY_PATH']
+
+    
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')

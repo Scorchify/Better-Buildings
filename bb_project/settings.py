9aae9b119f855920754ed2480c89d2584dce9484
Scorchify <139656419+Scorchify@users.noreply.github.com>
2024-08-26 22:16:55 -0400

    bug fix 3 for gdal

--- bb_project/settings.py
+++ bb_project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'django.contrib.gis',
 
     #pwa configuration
     'pwa',
